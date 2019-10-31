6cf666f9739734dd433b44ec89e3517d4b7803b8
Garrett Young <GarrettYoung510@Gmail.com>
2019-10-31 11:52:09 -0700

    delete comment functionality

--- routes/comments.js
+++ routes/comments.js
@@ -51,12 +51,52 @@ router.post("/", isLoggedIn, (req, res) => {
           comment.save();
           campground.comments.push(comment);
           campground.save();
-        //   console.log(comment);
+          //   console.log(comment);
           res.redirect(`/campgrounds/${campground._id}`);
         }
       });
       // connect new comment to campground
       // redirect campground show page
+    }
+  });
+});
+
+// comment edit route
+router.get("/:comment_id/edit", (req, res) => {
+  Comment.findById(req.params.comment_id, (err, foundComment) => {
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.render("comments/edit", {
+        campground_id: req.paramsid,
+        comment: foundComment
+      });
+    }
+  });
+});
+
+// comment update
+router.put("/:comment_id", (req, res) => {
+  Comment.findByIdAndUpdate(
+    req.params.comment_id,
+    req.body.comment,
+    (err, updatedComment) => {
+      if (err) {
+        res.redirect("back");
+      } else {
+        res.redirect("/campgrounds/" + req.params.id);
+      }
+    }
+  );
+});
+
+// comment destroy route
+router.delete("/:comment_id", (req, res) => {
+  Comment.findByIdAndRemove(req.params.comment_id, err => {
+    if (err) {
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
     }
   });
 });
